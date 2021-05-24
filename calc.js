120d3c09e30e5574d1c4f10f44b8e2b4d728f0bc
Joshua Mines <minesjoshua@gmail.com>
2021-05-24 15:22:00 -0600

    initial commit

--- calc.js
+++ calc.js
@@ -14,8 +14,28 @@ reader.question("What would you like to calculate?", function(input){
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
 
-
 	// This line closes the connection to the command line interface.
+
+	console.log('mathSymbol', mathSymbol);
+	console.log('num1', num1);
+	console.log('num2', num2);
+
+	if (mathSymbol === "+"){
+		console.log(num1+num2);
+		}
+	
+	if (mathSymbol === "-"){
+			console.log(num1-num2)
+		}
+	
+	if (mathSymbol === "*"){
+			console.log(num1*num2)
+	}
+
+	if (mathSymbol === "/"){
+			console.log(num1/num2)
+	}
+
 	reader.close()
 
 });
